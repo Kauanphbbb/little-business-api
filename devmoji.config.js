773b95ea52297f5d4d2d3e3de1d501b2f4ab29af
kauanphbbb <kauanphbbb@gmail.com>
2025-03-05 18:00:19 -0300

    feat: add husky hooks for linting and testing, configure commitlint and devmoji

---
+++ devmoji.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  types: ['commitlint'],
+  devmoji: [
+    { code: 'feat', emoji: 'crown' },
+    { code: 'fix', emoji: 'beetle' },
+    { code: 'docs', emoji: 'pencil' },
+    { code: 'style', emoji: 'art' },
+    { code: 'refactor', emoji: 'recycle' },
+    { code: 'perf', emoji: 'zap' },
+    { code: 'test', emoji: 'white_check_mark' },
+    { code: 'chore', emoji: 'necktie' },
+    { code: 'build', emoji: 'package' },
+    { code: 'ci', emoji: 'vertical_traffic_light' },
+    { code: 'lint', emoji: 'rewind' },
+    { code: 'revert', emoji: 'rewind' },
+  ],
+};
