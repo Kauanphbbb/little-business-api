023e11e30ee271351a2427563abf95028f0195c8
kauanphbbb <kauanphbbb@gmail.com>
2025-03-08 19:17:17 -0300

    feat: 👑 integrate dotenv for environment configuration and add class-transformer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
+import { env } from './shared/config/env';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.dbHost,
+      port: env.dbPort,
+      username: env.dbUser,
+      password: env.dbPassword,
+      database: env.dbName,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/shared/config/env.ts
@@ -0,0 +1,52 @@
+import { plainToInstance } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  NotEquals,
+  validateSync,
+} from 'class-validator';
+
+class Env {
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('your_secret_key')
+  jwtSecret: string;
+
+  @IsString()
+  @IsNotEmpty()
+  dbHost: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  dbPort: number;
+
+  @IsString()
+  @IsNotEmpty()
+  dbName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  dbUser: string;
+
+  @IsString()
+  @IsNotEmpty()
+  dbPassword: string;
+}
+
+export const env: Env = plainToInstance(Env, {
+  jwtSecret: process.env.JWT_SECRET,
+  dbHost: process.env.DB_HOST,
+  dbPort: parseInt(process.env.DB_PORT ?? '5432', 10),
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+});
+
+const errors = validateSync(env);
+
+console.log(env);
+
+if (errors.length > 0) {
+  throw new Error(JSON.stringify(errors, null, 2));
+}
