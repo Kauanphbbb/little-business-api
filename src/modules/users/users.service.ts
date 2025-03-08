51a06714291ba9bf4283aa60447ac0799da66a5b
kauanphbbb <kauanphbbb@gmail.com>
2025-03-08 17:13:20 -0300

    feat: 👑 implement user module with create and update DTOs, service, and controller

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    return `This action adds a new user`;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    console.log(updateUserDto);
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} user`;
+  }
+}
