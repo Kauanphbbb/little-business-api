8a8a278a6f9603d93e8c44f0899addc9ece9b1d2
kauanphbbb <kauanphbbb@gmail.com>
2025-03-09 14:09:15 -0300

    chore: 👔 disable unbound-method rule in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,6 +34,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/unbound-method': 'off',
     },
   },
 );
